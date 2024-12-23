8adda4146e9dab0a69b5c28cdf479e4a4e20842b
R0b0so <ethansagar222@gmail.com>
2024-12-22 18:42:46 -0800

    hello

---
+++ list.py
@@ -0,0 +1,9 @@
+empty_list = []
+print()
+list = [1, 2, 3, 4, 5]
+print(list)
+triple = [1, 2, 3] * 3
+print(triple)
+hundreds = [100, 200, 300, 400, 500]
+hundreds = hundreds[::-1]
+print(hundreds, "\n")
