73f52c633f8c64eff9c2a68e97a6cc42f1eb945c
R0b0so <ethansagar222@gmail.com>
2024-12-22 18:59:44 -0800

    hello

---
+++ Multiple_lists.py
@@ -0,0 +1,9 @@
+L = [2, 3, 10, 5, 1, 7, 0]
+print("Original List:", L)
+count = 0
+for i in L: 
+    count += i
+avg = count/len(L)
+L.sort
+print("Smallest element is: ", L[0])
+print("Largest element is: ", L[-1])
