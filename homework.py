8e7514db4b3084deed7681cd929b71fb20b441a4
R0b0so <ethansagar222@gmail.com>
2025-01-12 17:57:16 -0800

    hello

---
+++ homework.py
@@ -0,0 +1,5 @@
+number = 2 
+list1 = [1, 2, 3, 4, 5, 6, 7 ,8 , 9, 10]
+print("original list : ", list1)
+list1 = [1 ** number, 2 ** number, 3 ** number, 4 ** number, 5 ** number, 6 ** number, 7 ** number,8 ** number, 9 ** number, 10 ** number]
+print("squared list : ", list1)
